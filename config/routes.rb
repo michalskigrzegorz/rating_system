f2248b4ddedb9e23e279a50786945967a9424700
Grzegorz Michalski <grzegorz.michalski@invert8.pl>
2018-06-19 21:10:12 +0200

    patching routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'MainPage/index'
+  get 'main_page/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'MainPage/index'
+  root 'main_page#index'
 end
