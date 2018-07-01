7dffeda19075a875ae5475af15efc367061c9ad4
Grzegorz Michalski <grzegorz.michalski@invert8.pl>
2018-07-01 20:27:09 +0200

    adding bootstrap, jquery gems, adding main FEEDBACK template

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'main_page/index'
+  get 'feedback/index'
+
+  # get 'main_page/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'main_page#index'
+  root 'feedback#index'
 end
